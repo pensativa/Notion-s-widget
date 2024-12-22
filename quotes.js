255f6c4e981a6ddd35c8a3a8bea8e4cd3805a711
Mariia Opanasenko <mkofein@gmail.com>
2024-12-22 14:24:48 +0200

    Changed API source

--- quotes.js
+++ quotes.js
@@ -1,19 +1,21 @@
-// Powered by Quotable
-// https://github.com/lukePeavey/quotable
+// Powered by https://quotes-api-self.vercel.app/quote
+// https://github.com/well300/quotes-api
 
 document.addEventListener("DOMContentLoaded", () => {
     // DOM elements
     const button = document.querySelector(".quotes__button");
     const quote = document.querySelector(".quotes-item__text");
     const cite = document.querySelector(".quotes-item__author");
+    const api_url ="https://quotes-api-self.vercel.app/quote";
 
-    async function updateQuote() {
+    async function updateQuote(url) {
       try {
-        const response = await fetch("https://api.quotable.io/quotes/random");
-        const { statusCode, statusMessage, ...data } = await response.json();
+        const response = await fetch(url);
+        const data = await response.json();
         if (!response.ok) throw new Error(`${statusCode} ${statusMessage}`);
-        quote.textContent = data[0].content;
-        cite.textContent = data[0].author;
+        console.log(data);
+        quote.textContent = data.quote;
+        cite.textContent = data.author;
       } catch (error) {
         console.error(error);
         quote.textContent = "Opps... Something went wrong";
@@ -25,10 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
         button.style.rotate = '-360deg';
         setTimeout(() => {
             button.style.rotate = '0deg';
-            updateQuote();
+            updateQuote(api_url);
         }, 800);
     });
   
     // call updateQuote once when page loads
-    updateQuote();
+    updateQuote(api_url);
   });
